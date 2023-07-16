adceb3e9cd9adbad126e8c7f8c0dcbf8c908863c
naimulabeer <naimul.h.abeer@gmail.com>
2023-07-16 16:41:31 +0600

    handling new answers

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,8 @@ const initialState = {
   //'loading', 'error', 'ready', 'active'
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -33,13 +35,24 @@ function reducer(state, action) {
         ...state,
         status: "active",
       };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
     default:
       throw new Error("Action Unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -60,7 +73,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,11 @@
 import Option from "./Option";
 
-export default function Question({ question }) {
+export default function Question({ question, dispatch, answer }) {
   console.log(question);
   return (
     <div>
       <h4>{question.question}</h4>
-      <Option question={question} />
+      <Option question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 }
